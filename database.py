2a846cc32dcf8caffc37a4a72ab8741759f4c2dc
Prit Meghapara <pritmeghapara2516@gmail.com>
2025-07-10 09:05:50 +0530

    working_login

--- database.py
+++ database.py
@@ -1,9 +1,15 @@
 from sqlalchemy import create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()  # This loads variables from .env into environment
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+# print(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
